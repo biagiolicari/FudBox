05c667fe3bd90aff02b5a6519bd4bb12fb3cb6dc
Biagio Licari <biagiolicari@outlook.com>
2023-08-20 20:29:21 +0200

    Firebase repository in respect of MVVM

---
+++ app/src/main/java/com/andorid/fudbox/repository/loginRepo/LoginRepository.java
@@ -0,0 +1,65 @@
+package com.andorid.fudbox.repository.loginRepo;
+
+import android.app.Application;
+import android.widget.Toast;
+import androidx.lifecycle.MutableLiveData;
+import com.andorid.fudbox.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginRepository {
+
+    private FirebaseAuth firebaseAuth;
+
+    private Application application;
+    private MutableLiveData<FirebaseUser> userLiveData;
+    private MutableLiveData<Boolean> loggedOutLiveData;
+
+    public LoginRepository(Application application){
+        this.application = application;
+        this.firebaseAuth = FirebaseAuth.getInstance();
+        this.userLiveData = new MutableLiveData<>();
+        this.loggedOutLiveData = new MutableLiveData<>();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            userLiveData.postValue(firebaseAuth.getCurrentUser());
+            loggedOutLiveData.postValue(false);
+        }
+    }
+
+    public void login(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(application.getMainExecutor(), task -> {
+                    if (task.isSuccessful()) {
+                        userLiveData.postValue(firebaseAuth.getCurrentUser());
+                    } else {
+                        Toast.makeText(application.getApplicationContext(), "Login Failure: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void register(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(application.getMainExecutor(), task -> {
+                    if (task.isSuccessful()) {
+                        userLiveData.postValue(firebaseAuth.getCurrentUser());
+                    } else {
+                        Toast.makeText(application.getApplicationContext(), "Registration Failure: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void logOut() {
+        firebaseAuth.signOut();
+        loggedOutLiveData.postValue(true);
+    }
+
+    public MutableLiveData<FirebaseUser> getUserLiveData(){
+        return userLiveData;
+    }
+
+    public MutableLiveData<Boolean> getLoggedOutLiveData(){
+        return loggedOutLiveData;
+    }
+
+}
