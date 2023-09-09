87bd8df306769ec878a8bd3969bea1d8287a6a81
Biagio Licari <biagiolicari@outlook.com>
2023-09-09 20:57:13 +0200

    Fixed small things. Introduced sharedViewModel to share data and to fix error when back to home fragment from payfragment

--- app/src/main/java/com/andorid/fudbox/view/mainscreen/home/HomeFragment.java
+++ app/src/main/java/com/andorid/fudbox/view/mainscreen/home/HomeFragment.java
@@ -19,6 +19,7 @@ import com.andorid.fudbox.databinding.FragmentHomePageBinding;
 import com.andorid.fudbox.model.Restaurant;
 import com.andorid.fudbox.model.restaurant.RestaurantFeature;
 import com.andorid.fudbox.viewmodel.mainscreen.home.RestaurantViewModel;
+import com.andorid.fudbox.viewmodel.mainscreen.shared.SharedLatLng;
 import com.google.android.gms.maps.model.LatLng;
 
 
@@ -29,6 +30,7 @@ public class HomeFragment extends Fragment implements RestaurantSearchResultAdap
     private RecyclerView recyclerView;
     private FragmentHomePageBinding binding;
     private ProgressBar progressBar;
+    private SharedLatLng sharedLatLng;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -38,11 +40,14 @@ public class HomeFragment extends Fragment implements RestaurantSearchResultAdap
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = new ViewModelProvider(this).get(RestaurantViewModel.class);
+        /**
         Bundle args = getArguments();
         if (args != null) {
             latLngData = new LatLng(args.getDouble("latitude"), args.getDouble("longitude"));
         }
+         **/
         viewModel.init();
+        sharedLatLng = new ViewModelProvider(requireActivity()).get(SharedLatLng.class);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,11 +59,13 @@ public class HomeFragment extends Fragment implements RestaurantSearchResultAdap
         // Initialize RecyclerView and Adapter
         recyclerView = binding.fragmentRestaurantSearchResultsRecyclerView;
         recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
-        adapter = new RestaurantSearchResultAdapter(requireContext(), this, latLngData);
-        recyclerView.setAdapter(adapter);
+        sharedLatLng.getLatLngMutableLiveData().observe(getViewLifecycleOwner(), latLng -> {
+            adapter = new RestaurantSearchResultAdapter(requireContext(), this, latLng);
+            recyclerView.setAdapter(adapter);
+            //search restaurants
+            viewModel.searchRestaurants(latLng);
+        });
 
-        //search restaurants
-        viewModel.searchRestaurants(latLngData);
 
         viewModel.getRestaurantsLiveData().observe(getViewLifecycleOwner(), restaurantFeatures -> {
             adapter.setRestaurants(restaurantFeatures);
--- app/src/main/java/com/andorid/fudbox/view/mainscreen/order/OrderPaymentFragment.java
+++ app/src/main/java/com/andorid/fudbox/view/mainscreen/order/OrderPaymentFragment.java
@@ -8,9 +8,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.andorid.fudbox.R;
 import com.andorid.fudbox.databinding.FragmentOrderPaymentBinding;
 import com.andorid.fudbox.viewmodel.mainscreen.order.OrderViewModel;
+import com.rejowan.cutetoast.CuteToast;
 
 public class OrderPaymentFragment extends Fragment {
     private OrderViewModel orderViewModel;
@@ -19,6 +24,8 @@ public class OrderPaymentFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        orderViewModel = new ViewModelProvider(requireActivity()).get(OrderViewModel.class);
+        orderViewModel.init();
     }
 
     @Nullable
@@ -26,11 +33,20 @@ public class OrderPaymentFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentOrderPaymentBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+
+        binding.payButton.setOnClickListener(l ->  paymentCompleted());
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void paymentCompleted(){
+        orderViewModel.uploadOrderToFirestore();
+        CuteToast.ct(requireContext(), getString(R.string.order_completed), CuteToast.LENGTH_SHORT, CuteToast.HAPPY, true).show();
+        NavController navController = Navigation.findNavController(requireView());
+        navController.navigate(R.id.action_return_to_home);
     }
 }
