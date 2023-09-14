d2e6a19a128ad530a25d1d0b36c0cd48a9732b13
Biagio Licari <biagiolicari@outlook.com>
2023-09-14 16:49:03 +0200

    Reintroduced random dish selection for creating the menu. Maybe it is a bit computational expensive

--- app/src/main/java/com/andorid/fudbox/repository/mainscreen/home/menu/DishRepository.java
+++ app/src/main/java/com/andorid/fudbox/repository/mainscreen/home/menu/DishRepository.java
@@ -2,6 +2,7 @@ package com.andorid.fudbox.repository.mainscreen.home.menu;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
 import com.andorid.fudbox.model.Dish;
@@ -12,6 +13,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -38,11 +40,22 @@ public class DishRepository {
         firestore.collection(COLLECTION)
                 .get()
                 .addOnSuccessListener(queryDocumentSnapshots -> {
-                    List<Dish> dishList = new ArrayList<>();
+                    List<Dish> completeDishListFromFirestore = new ArrayList<>();
                     for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                        dishList.add(this.createDishFromDocumentSnapshot(documentSnapshot));
+                        completeDishListFromFirestore.add(this.createDishFromDocumentSnapshot(documentSnapshot));
                     }
-                    dishListLiveData.setValue(Resource.success(dishList));
+
+                    completeDishListFromFirestore.forEach(dish -> Log.wtf("DISH", dish.getName()));
+
+                    List<Dish> tmp = completeDishListFromFirestore.stream()
+                            .collect(Collectors.groupingBy(Dish::getType))
+                            .values()
+                            .stream()
+                            .flatMap(sublist -> pickRandom(sublist).stream()) // Cambia 3 con il numero desiderato di piatti casuali per ogni chiave
+                            .collect(Collectors.toList());
+
+                    Collections.sort(tmp, Comparator.comparing(Dish::getType));
+                    dishListLiveData.setValue(Resource.success(tmp));
                 })
                 .addOnFailureListener(e -> dishListLiveData.setValue(Resource.error(e.getMessage(), null)));
     }
@@ -65,6 +78,23 @@ public class DishRepository {
     private int generateRandomIndexNumber(List itemList){
         // Generate a random index between 1 and list.size()
         return new Random().nextInt(itemList.size());
+    }
+
+    private static <E> List<E> pickRandom(@NonNull List<E> list, int n) {
+        return new Random().ints(n, 0, list.size()).mapToObj(list::get).collect(Collectors.toList());
+    }
+
+    private static <E> List<E> pickRandom(List<E> list) {
+        if (list == null || list.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        Random random = new Random();
+        int n = random.nextInt(list.size()) + 1; // Genera un numero casuale tra 1 e la dimensione della lista
+
+        return random.ints(0, list.size()).distinct().limit(n)
+                .mapToObj(list::get)
+                .collect(Collectors.toList());
     }
 
 }
